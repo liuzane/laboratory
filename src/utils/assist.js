349e9e3d4e12b26797112fa4dd572f2568b410d3
liuzheng <Zheng.Liu@b-and-qchina.com>
2020-12-10 18:13:12 +0800

    更新assist.js

--- src/utils/assist.js
+++ src/utils/assist.js
@@ -44,8 +44,9 @@ export function deepCopy (data) {
   return object;
 }
 
+// 生成 UUID
 export function uuid() {
-  return 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
 
@@ -53,6 +54,7 @@ export function uuid() {
   });
 }
 
+// 下载
 export function download(name, url) {
   const aDom = document.createElement('a');
   aDom.download = name;
@@ -60,4 +62,45 @@ export function download(name, url) {
   document.body.appendChild(aDom);
   aDom.click();
   aDom.remove(); // 下载之后把创建的元素删除
+}
+
+// 将路径参数转化为对象格式
+export function getUrlQuery(url) {
+  const query = url.substring(url.indexOf('?') + 1).split('&');
+  const params = {};
+  query.forEach(item => {
+    const key_value = item.split('=');
+    params[key_value[0]] = key_value[1];
+  });
+  return params;
+}
+
+// 将对象格式转化为路径参数
+export function setUrlQuery(params) {
+  let querys = [];
+  for (const key in params) {
+    if (params.hasOwnProperty(key)) {
+      querys.push(key + '=' + params[key]);
+    }
+  }
+  return querys.join('&');
+}
+
+// 获取对象深层值
+export function getDeepValueOfObject(object, keyPath, defaultValue) {
+  if (object) {
+    const keys = keyPath.split(',');
+    let value = object;
+    for (let index = 0; index < keys.length; index++) {
+      const key = keys[index];
+      if (Boolean(value[key])) {
+        value = value[key];
+      } else {
+        value = defaultValue;
+      }
+    }
+    return value;
+  } else {
+    return defaultValue;
+  }
 }
