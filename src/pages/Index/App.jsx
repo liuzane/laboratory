dfd6c579f1f5e4022429c11da51f92a506efd5c7
liuzheng <Zheng.Liu@b-and-qchina.com>
2021-08-02 20:15:02 +0800

    首页添加logo

--- src/pages/Index/App.jsx
+++ src/pages/Index/App.jsx
@@ -1,25 +1,53 @@
 // 基础模块
 import React, { PureComponent } from 'react';
 
+// 样式
+import './App.css';
+
 
 export default class App extends PureComponent {
   constructor(props) {
     super(props);
+    this.entrances = [
+      {
+        title: 'Admin',
+        href: '/admin.html',
+        img: '',
+      },
+      {
+        title: 'React',
+        href: '/react.html',
+        img: require('./images/react-logo.png'),
+      },
+      {
+        title: 'Vue',
+        href: '/vue.html',
+        img: require('./images/vue-logo.png'),
+      }
+    ];
   }
 
   render() {
     return (
       <div className="index">
-        <ul>
-          <li>
-            <a href="/admin.html">Admin</a>
-          </li>
-          <li>
-            <a href="/react.html">React</a>
-          </li>
-          <li>
-            <a href="/vue.html">Vue</a>
-          </li>
+        <ul className="index__entrance">
+          {
+            this.entrances.map((item, index) => (
+              <li key={ index } className="index__entrance-item">
+                <a href={ item.href } className="index__entrance-link">
+                  {
+                    item.img
+                      ? <img
+                        src={ item.img }
+                        alt={ item.title }
+                        className="index__entrance-img"
+                      />
+                      : <span>{ item.title }</span>
+                  }
+                </a>
+              </li>
+            ))
+          }
         </ul>
       </div>
     );
